6df1c036930c751bf9c7501c10506fae7294921e
George Lvov <lvovgeorge@gmail.com>
2025-11-24 15:49:14 +0200

    Initial commit

---
+++ spring-ai-mcp-client/src/main/java/com/glvov/springaimcpclient/SpringAiMcpClientApplication.java
@@ -0,0 +1,13 @@
+package com.glvov.springaimcpclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAiMcpClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAiMcpClientApplication.class, args);
+    }
+
+}
